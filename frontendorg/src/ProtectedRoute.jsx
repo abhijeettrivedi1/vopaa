a8a7fcff00642e5dc5a9b5b28476fae277ea60b5
abhijeettrivedi1 <127127641+abhijeettrivedi1@users.noreply.github.com>
2024-08-19 00:44:42 +0530

    Update ProtectedRoute.jsx

--- frontendorg/src/ProtectedRoute.jsx
+++ frontendorg/src/ProtectedRoute.jsx
@@ -1,10 +1,29 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { Usercontext } from "./usercontext";
+import { Oval } from 'react-loader-spinner';
+
 
 const ProtectedRoute = ({ children }) => {
-  const { user } = useContext(Usercontext);
+  const { user, ready } = useContext(Usercontext);  
   console.log(user)
+  if (!ready) {
+    return  <div className="spinner-container">
+    <Oval
+        height={80}
+        width={80}
+        color="#4fa94d"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        ariaLabel="oval-loading"
+        secondaryColor="#4fa94d"
+        strokeWidth={2}
+        strokeWidthSecondary={2}
+    />
+</div>
+}
+console.log(user)
   if (!user) {
     return <Navigate to="/login" />;
   }
